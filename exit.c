e69f7f8de58405a6c0c0d2b14b20e23a7282586d
roni2706 <roni2706@gmail.com>
2017-04-27 23:44:03 +0300

    fix

--- exit.c
+++ exit.c
@@ -395,21 +395,21 @@ static void exit_notify(void)
 {
 	struct task_struct * p, *t;
 /* an extra one on purpose to avoid messing with stuff we dont want to know about*/
-	struct task_struct * parent_proc;  
+	struct task_struct* parent_proc;
 	parent_proc = current->p_pptr;
 	if(parent_proc->max_zombies!=-1){
 		zombie_list new_zombie_node ;
 		new_zombie_node->pid= current->pid;
-		list_add_tail(&(new_zombie_node->list),&(parent_proc->zombies));
+		list_add_tail(&(new_zombie_node->list),parent_proc->zombies);
 		parent_proc->curr_zombies ++;
 		task_t* init_ptr = find_task_by_pid(1);
 		if (init_ptr->max_zombies!=-1){
 			if (current->curr_zombies + init_ptr->curr_zombies <= init_ptr->max_zombies){
-				struct list_head *prev,*pos=current->zombies->(&list);
+				struct list_head *prev,*pos=&(current->zombies->list);
 				for(int i=0;i<current->curr_zombies;i++){
 					prev=pos;
 					pos=pos->next;
-					list_move_tail(prev,init_ptr->zombies->(&list));
+					list_move_tail(prev,&(init_ptr->zombies->list));
 				}
 				init_ptr->curr_zombies+=curr_zombies-current->curr_zombies;
 			}
